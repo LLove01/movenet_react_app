2b0f0d4c02af40d405ff7a17a788246533fc385c
LLove01 <luka.lovenjak44@gmail.com>
2024-01-13 11:56:03 +0100

    less delay, bigger angle display

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useRef, useState, useEffect, useCallback } from 'react';
 import Webcam from 'react-webcam';
 import * as tf from '@tensorflow/tfjs';
 import * as poseDetection from '@tensorflow-models/pose-detection';
+// import '@tensorflow/tfjs-backend-wasm';
 import { drawKeypoints, drawSkeletonLines, calculateAngle } from './utilities';
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
   const canvasRef = useRef(null);
   const [model, setModel] = useState(null);
   const [isCameraActive, setIsCameraActive] = useState(false);
-  const [drawSkeleton, setDrawSkeleton] = useState(true); // State to control skeleton drawing
+  const [drawSkeleton, setDrawSkeleton] = useState(false);
+  const [isLoadingModel, setIsLoadingModel] = useState(true);
+
 
   // Angle state varibles
   const [showLeftKneeAngle, setShowLeftKneeAngle] = useState(false);
@@ -21,16 +24,23 @@ function App() {
   const [angleRightKnee, setRightKneeAngle] = useState(null);
   const [angleLeftHip, setLeftHipAngle] = useState(null);
   const [angleRightHip, setRightHipAngle] = useState(null);
+  // Counter 
+  const [squatCount, setSquatCount] = useState(0);
+  const [isInSquatPosition, setIsInSquatPosition] = useState(false);
+
 
 
   // Load MoveNet model
   useEffect(() => {
     const loadModel = async () => {
+      setIsLoadingModel(true);
+      // await tf.setBackend('wasm');
       await tf.setBackend('webgl');
       await tf.ready();
       const detectorConfig = { modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING };
       const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
       setModel(detector);
+      setIsLoadingModel(false);
     };
     loadModel();
   }, []);
@@ -38,6 +48,7 @@ function App() {
   function drawResults(poses) {
     const ctx = canvasRef.current.getContext("2d");
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.font = "20px Arial";
 
     for (const pose of poses) {
       if (drawSkeleton) {
@@ -83,12 +94,20 @@ function App() {
             ctx.fillText(` ${angleRightHip.toFixed(0)}°`, rightHip.x, rightHip.y);
           }
         }
-        // Inside drawResults function
         setLeftKneeAngle(angleLeftKnee);
         setRightKneeAngle(angleRightKnee);
         setLeftHipAngle(angleLeftHip);
         setRightHipAngle(angleRightHip);
 
+        if (angleLeftKnee <= 90 && angleRightKnee <= 90) {
+          if (!isInSquatPosition) {
+            setIsInSquatPosition(true);
+          }
+        } else if (isInSquatPosition) {
+          // Person is coming up from a squat
+          setIsInSquatPosition(false);
+          setSquatCount(prevCount => prevCount + 1); // Increment squat count
+        }
       }
     }
   }
@@ -136,7 +155,7 @@ function App() {
 
       const interval = setInterval(() => {
         detectPose();
-      }, 100);
+      }, 10);
       return () => clearInterval(interval);
     }
   }, [isCameraActive, detectPose]);
@@ -144,6 +163,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {isLoadingModel && <p>Loading model, please wait...</p>}
         <div style={{ position: 'relative', width: '640px', height: '480px' }}>
           {isCameraActive && <Webcam ref={webcamRef} style={{ width: '100%', height: '100%' }} />}
           <canvas ref={canvasRef} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} />
@@ -172,6 +192,7 @@ function App() {
             Right Hip
           </label>
         </div>
+        <p>Squat Count: {squatCount}</p>
       </header>
     </div>
   );
